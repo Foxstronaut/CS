876a902a9e067459d75cdb2877eb516a03b1f9f0
Elliott Weston Ball <elliottwball@gmail.com>
2026-01-30 10:25:01 -0800

    eeeeeeeeeeee

--- 1-30/DebugLab2.java
+++ 1-30/DebugLab2.java
@@ -1,16 +1,15 @@
 public class DebugLab2 {
-
     public static void main(String[] args) {
-    String word = "metamorphosis";
-    int i = 0;
-    while(i < word.length() - 1) {
-        String letter = word.substring(i, i + 1);
-        if (letter.equals("m")) {
-            i += 2;
-        } else if (letter.equals("i")) {
-            i += 3;
-        }
-        i++;
-    } 
-}
+        String word = "metamorphosis";
+        int i = 0;
+        while(i < word.length() - 1) {
+            String letter = word.substring(i, i + 1);
+            if (letter.equals("m")) {
+                i += 2;
+            } else if (letter.equals("i")) {
+                i += 3;
+            }
+            i++;
+        } 
+    }
 }
--- 1-30/DebuggerLab.java
+++ 1-30/DebuggerLab.java
@@ -1,11 +1,11 @@
 public class DebuggerLab {
     public static void main(String[] args) {
-        String word = "Java";
+        String word1 = "Java";
         String reversed = "";
 
         // Goal: Reverse the string so it prints "avaJ"
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed = reversed + word.charAt(i);
+        for (int i = word1.length() - 1; i >= 0; i--) {
+            reversed = reversed + word1.charAt(i);
         }
 
         System.out.println("Result: " + reversed);
