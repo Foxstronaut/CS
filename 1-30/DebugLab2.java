8add6d90a98cccf07dff05bae297fa7463315ab4
Elliott Weston Ball <elliottwball@gmail.com>
2026-01-30 10:21:05 -0800

    HHH

--- 1-30/DebugLab2.java
+++ 1-30/DebugLab2.java
@@ -1,9 +1,7 @@
 public class DebugLab2 {
-    public static void main(String[] args) {
-        jumpAndHop("metamorphism");
-    }
 
-    public static void jumpAndHop(String word) {
+    public static void main(String[] args) {
+    String word = "metamorphosis";
     int i = 0;
     while(i < word.length() - 1) {
         String letter = word.substring(i, i + 1);
