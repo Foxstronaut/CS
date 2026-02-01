3fc9a63acd9c66c6a2df0cd696f6b979bef93dd3
Elliott Weston Ball <elliottwball@gmail.com>
2026-02-01 12:38:59 -0800

    monstermath

---
+++ monstermath.java
@@ -0,0 +1,7 @@
+public class monstermath {
+    public static void main(String[] args) {
+        import java.util;
+        Scanner input = new Scanner;
+        System.out.println("hello user, how much $ do you have: ");
+    }
+}
